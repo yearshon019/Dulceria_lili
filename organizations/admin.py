10523bf7e7f2b968a0dd8f1cf912e78172ab3739
yearshon019 <yearshon.yoain.orrego@gmail.com>
2025-09-23 12:04:26 +0100

    te toca joven

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Organization
 
-# Register your models here.
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    search_fields = ("name",)
